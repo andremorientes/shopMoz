210306b2b1a05de5e38633f6527da9e5b266b84d
André Morientes <59062594+andremorientes@users.noreply.github.com>
2024-07-08 20:24:39 +0200

    TODO: CONCLUIR A CONFIGURAÇÃO DO CONTROLLER  NAV BOTTOM

--- app/src/main/java/com/example/shopmoz/activitys/ShoppingActivity.kt
+++ app/src/main/java/com/example/shopmoz/activitys/ShoppingActivity.kt
@@ -2,11 +2,23 @@ package com.example.shopmoz.activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.shopmoz.R
+import com.example.shopmoz.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottomNavigation.setupWithNavController(navController)
+
     }
 }
---
+++ app/src/main/java/com/example/shopmoz/fragments/shopping/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.example.shopmoz.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.shopmoz.R
+
+class ProfileFragment: Fragment(R.layout.fragment_profile) {
+}
